c0a10ab73b1d7c6c5232222c7fab60d66b15c6eb
Gabriel Rodriguez <90540200+Gaberod91@users.noreply.github.com>
2023-01-24 22:58:21 -0500

    Updated

---
+++ UpdateContact.php
@@ -0,0 +1,42 @@
+<?php
+	$inData = getRequestInfo();
+
+    $id = $inData["ID"];
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+    $email = $inData["email"];
+    $phone = $inData["phone"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName = ?, Email = ?, Phone = ? WHERE ID = ?");
+		$stmt->bind_param("ssssi", $firstName, $lastName, $email, $phone, $id);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
