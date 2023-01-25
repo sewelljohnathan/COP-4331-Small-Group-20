8999569abfd43c963c9b39530487a09605c6877b
Gabriel Rodriguez <90540200+Gaberod91@users.noreply.github.com>
2023-01-24 22:10:37 -0500

    Updated

--- CreateAccount.php
+++ CreateAccount.php
@@ -3,8 +3,8 @@
 
 	$firstName = $inData["firstName"];
 	$lastName = $inData["lastName"];
-    $login = $inData["login"];
-    $password = $inData["password"];
+        $login = $inData["login"];
+        $password = $inData["password"];
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error) 
