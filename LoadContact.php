9cc65c88f7cfbd042011acb8451b4f7857f3df35
sonoma15 <zacharykappitt@gmail.com>
2023-01-26 22:44:26 -0500

    added loadcontact.phh
    should load all contacts if works correctly we will see ig

---
+++ LoadContact.php
@@ -0,0 +1,42 @@
+<?php
+    $inData = getRequestInfo();
+
+    $id = $inData["ID"];
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+    $email = $inData["email"];
+    $phone = $inData["phone"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("select FirstName, LastName, Email, PhoneNumber FROM Contacts WHERE ID = ?");
+		$stmt->bind_param("i", $id);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
